4bd7d6655c877de7bf9f89c8f338d2e830ca7a04
vesatukio <vesatukio@gmail.com>
2026-05-01 19:55:57 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,4 @@
-const CACHE_NAME = "dt-led-v1";
-
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+// Service Worker Minimalis
+self.addEventListener('fetch', (event) => {
+  // Biarkan browser melakukan fetch standar
 });
