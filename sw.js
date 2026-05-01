42099a30747630e0b06684638bb4a57e487f7ea9
vesatukio <vesatukio@gmail.com>
2026-05-01 19:00:41 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,51 @@
-const CACHE_NAME = "dtled-v3";
+const CACHE_NAME = "dtled-v4";
 
-self.addEventListener("install", e=>{
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html"
+];
+
+/* INSTALL */
+self.addEventListener("install", e => {
+  self.skipWaiting(); // langsung aktif
+
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache=>{
-      return cache.addAll(["./","./index.html"]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
 });
 
-self.addEventListener("fetch", e=>{
-  e.respondWith(
-    caches.match(e.request).then(res=>{
-      return res || fetch(e.request);
+/* ACTIVATE (BERSIHIN CACHE LAMA) */
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
     })
+  );
+
+  self.clients.claim();
+});
+
+/* FETCH (PAKSA UPDATE DARI SERVER DULU) */
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(e.request, clone);
+        });
+
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
